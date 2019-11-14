20b07b04e5d6c51ccff9712ea82fc10fbf86dba8
Elyalain <sarmientoelizabeth133@gmail.com>
2019-11-13 19:27:52 -0500

    heyy

--- index.js
+++ index.js
@@ -98,8 +98,12 @@ Car.prototype.drive=function(distance){
         + Should return a string "Playing with x", x being the favorite toy.
 */
 function Baby(name,age,favoriteToy) {
-
+this.favoriteToy=favoriteToy
 }
+Baby.prototype= Object.create(Person.prototype);
+Baby.prototype.play= function(){
+  return 'Playing with ${this.favoriteToy}';
+
 
 /* 
   TASK 4
